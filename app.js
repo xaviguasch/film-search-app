23239d0db73daa8f36ee11d1402760c14e6a476e
xaviguasch <xaviguasch@gmail.com>
2019-01-03 10:49:20 +0100

    displaying the results data

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const request = require('request')
 
+app.set('view engine', 'ejs')
+
 app.get('/results', function(req, res) {
-    request('http://www.omdbapi.com/?s=california&tomatoes&apikey=thewdb', function(error, response, body) {
+    request('http://www.omdbapi.com/?s=iowa&tomatoes&apikey=thewdb', function(error, response, body) {
         if (!error && response.statusCode == 200) {    
-            const results = JSON.parse(body);
-            res.send(results['Search'][0]['Title'])
+            const data = JSON.parse(body);
+            res.render('results', {data: data})
         }
     })
 })
