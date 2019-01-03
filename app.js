3c7d9bb4104cd98eae5c20fd93e414704fc1567c
xaviguasch <xaviguasch@gmail.com>
2019-01-03 11:37:44 +0100

    adding search option and displaying its results

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const request = require('request')
 
 app.set('view engine', 'ejs')
 
+app.get('/', function(req, res) {
+    res.render('search')
+})
+
 app.get('/results', function(req, res) {
-    request('http://www.omdbapi.com/?s=iowa&tomatoes&apikey=thewdb', function(error, response, body) {
+    const query = req.query.search
+    const url = `http://www.omdbapi.com/?s=${query}&apikey=thewdb`
+    request(url, function(error, response, body) {
         if (!error && response.statusCode == 200) {    
-            const data = JSON.parse(body);
+            const data = JSON.parse(body)
             res.render('results', {data: data})
         }
     })
